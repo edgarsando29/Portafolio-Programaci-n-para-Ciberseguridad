1de8f1578e971f9bd4caa602fa54fbed87cf65c7
edgarsando29 <edgar.sandovalszr@uanl.edu.mx>
2021-11-12 01:18:08 -0600

    .

--- E13/getfirewallprofile.ps1
+++
@@ -1,9 +0,0 @@
-﻿param([Parameter(Mandatory)] [ValidateSet("Public","Private")] [string] $perfil)
-$status = Get-NetFirewallProfile -Name $perfil  
-Write-Host "Perfil:" $perfil
-if($status.enabled){  
-    Write-Host "Status: Activado"  
-} 
-else{  
-    Write-Host "Status: Desactivado"  
-}
